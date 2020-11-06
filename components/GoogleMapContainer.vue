0d033a9a686072505b4f82d266b9abb3687c8596
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-06 12:16:06 +0200

    add intial component to contain the google map

---
+++ components/GoogleMapContainer.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="container">
+  <div class="map" ref="map">
+
+  </div>
+</div>
+</template>
+
+<script>
+import {Loader} from "@googlemaps/js-api-loader"
+
+export default {
+  name: "GoogleMapContainer",
+  data() {
+    return {
+      map: null,
+
+    }
+  },
+  mounted() {
+    const loader = new Loader({
+      apiKey: "AIzaSyB94wtYbbU7JEsO8XIF9sJpMTAlBsBVti0",
+      version: "weekly",
+    });
+    loader.load().then(() => {
+      this.map = new google.maps.Map(this.$refs.map, {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 10,
+      });
+    });
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.map {
+  width: 80vw;
+  height: 400px;
+}
+</style>
